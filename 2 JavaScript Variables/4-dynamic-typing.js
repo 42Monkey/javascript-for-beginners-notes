679062f5f76ac1e9ff2ff19b54b16da85ffbc21e
Tony <160587075+42Monkey@users.noreply.github.com>
2024-11-03 16:10:28 +0700

    Update 4-dynamic-typing.js

--- 2 JavaScript Variables/4-dynamic-typing.js
+++ 2 JavaScript Variables/4-dynamic-typing.js
@@ -1,14 +1,16 @@
-// JavaScript is dynamically typed.
-// Meaning that you can initialize a variable to a particular data type.
-// Then you can reassign it later to a different data type.
-// Programming languages which are statically typed, such as Java or C#,
-// do not allow you to do this.
+// JavaScript is a dynamically typed language.
+// This means that a variable can hold any data type, and you can change
+// its type by assigning a new value of a different type to it.
 
-let firstName = 'Steven';
-console.log(typeof firstName);
+// In statically typed languages like Java or C#, each variable has a fixed type,
+// so you can't change its type after it's set.
 
-firstName = 100;
-console.log(typeof firstName);
+// Example:
+let firstName = 'Steven';			// firstName is a string
+console.log(typeof firstName);		// shows 'string'
 
-firstName = true;
-console.log(typeof firstName);
+firstName = 100;					// new firstName is a number 
+console.log(typeof firstName);		// shows 'number'
+
+firstName = true;					// now firstName is a boolean
+console.log(typeof firstName);		// shows 'boolean'
