bc6a8d329eddfc83cda38de9108758036fce936d
Tony <160587075+42Monkey@users.noreply.github.com>
2024-11-03 17:55:53 +0700

    Update 7-logical-operators.js

--- 3 JavaScript Operators/7-logical-operators.js
+++ 3 JavaScript Operators/7-logical-operators.js
@@ -1,65 +1,61 @@
-// We use logical operators to make decisions based on multiple conditions.
-// There are 4 logical operators which include:
-// || (or operator)
-// && (and operator)
-// !  (not operator)
-// ?? (null coalesing)
+// Logical operators are used to make decisions based on multiple conditions.
+// There are four main logical operators:
+// 1. || (or operator)
+// 2. && (and operator)
+// 3. !  (not operator)
+// 4. ?? (nullish coalescing operator)
 
-// These can be applied to values of any type, not just boolean values.
+// These operators can be applied to values of any type, not just boolean values.
 // Expressions are evaluated from left to right.
 
-// Consider the four possible logical combinations that can be made when working with two operands.
-// So as long as one is true, then it will return true.
-console.log(true || true); // true
-console.log(true || false); // true
-console.log(false || true); // true
-console.log(false || false); // false
+// The || operator (or operator)
+// Returns true if at least one of the operands is true.
+console.log(true || true);							// true
+console.log(true || false);							// true
+console.log(false || true);							// true
+console.log(false || false);						// false
 
-// An example of the ||, or operator 
+// Example using the || operator
 let hasReservation = true;
 let acceptingWalkIns = false;
 const hasAccessToTable = hasReservation || acceptingWalkIns;
+console.log('hasAccessToTable', hasAccessToTable);	// outputs: hasAccessToTable true
 
-console.log('hasAccessToTable', hasAccessToTable);
+// The && operator (and operator)
+// Returns true only if all operands are truthy.
+console.log(true && true);							// true
+console.log(true && false);							// false
+console.log(false && true);							// false
+console.log(false && false);						// false
 
-// &&, the AND operator
-// This returns true if all the operands being evaluated are truthy.
-// Consider the four possible logical combinations that can be made when working with two operands.
-console.log(true && true); // true
-console.log(true && false); // false
-console.log(false && true); // false
-console.log(false && false); // false
-
+// Example using the && operator
 let age = 16;
 let hasCar = true;
 const canDrive = age >= 16 && hasCar;
+console.log('canDrive', canDrive); 					// outputs: canDrive true
 
-console.log('canDrive', canDrive);
-
-// Another example
+// Another example combining && and || operators
 let a = true, b = true, c = true, d = true;
+console.log(a && b || c && d);						// evaluates to true
 
-console.log(a && b || c && d);
+// This is equivalent to:
+console.log((a && b) || (c && d));					// evaluates to true
 
-// Same as...
-console.log((a && b) || (c && d));
+// The ! operator (not operator)
+// Returns the inverse of the operand.
+console.log(!true);									// false
 
-// !, the NOT operator
-// This will return the inverse of the operand.
-console.log(!true); // false
-
-// Another example
+// Example using the ! operator
 let isClosedOnSunday = true;
+const isRestaurantOpen = !isClosedOnSunday;			// false
+console.log('isRestaurantOpen', isRestaurantOpen);	// outputs: isRestaurantOpen false
 
-const isRestaurantOpen = !isClosedOnSunday;
 
-console.log(isReservaationOpen);
-
-// ??, nullish coalescing operator
-// So if a value is null or undefined, then we can supply a default value.
-
+// The ?? operator (nullish coalescing operator)
+// Returns the right-hand side value when the left-hand side value is null or undefined
 let doesValueExist = null;
-const result = doesValueExist ?? false;
+const result = doesValueExist ?? false;				// false
+console.log('result', result);						// outputs: result false
 
 // So the ?? operator is syntactic sugar for...
-const resultOfExpression = (a !== null && a !== undefined) ? a : false;
+const resultOfExpression = (doesValueExist !== null && doesValueExist !== undefined) ? doesValueExist : false;
