171240082a6daf4b3b9d2b2c42406be9fa0f8f58
Tony <160587075+42Monkey@users.noreply.github.com>
2024-11-03 17:25:50 +0700

    Update 2-arithmetic-operators.js

--- 3 JavaScript Operators/2-arithmetic-operators.js
+++ 3 JavaScript Operators/2-arithmetic-operators.js
@@ -1,38 +1,42 @@
-// An expression in JavaScript is something that produces a value.
-// An expression goes on the right side of our variable assignment.
+// An expression in JavaScript is any code that produces a value.
+// Expressions typically appear on the right side of a variable assignment.
 
-// In JavaScript, we have different arithmetic operators.
-// -, +, *, /, % 
+// JavaScript has different arithmetic operators for performing calculations:
+// -, +, *, / and % 
 
-// % is the modulo operator and it returns the remainder of division.
+// The % operator is called the modulo operator. It returns the remainder of a division.
 
-console.log(2 + 2); // 4
+console.log(2 + 2); // outputs 4 (addition)
 
-console.log(4 - 2); // 2
+console.log(4 - 2); // outputs 2 (subtraction)
 
-console.log(2 * 2); // 4
+console.log(2 * 2); // outputs 4 (multiplication)
 
-console.log(9 / 3); // 3
+console.log(9 / 3); // outputs 3 (division)
 
-console.log(10 % 2); // the remainder of 0
+console.log(10 % 2); // outputs 0 (the remainder of 10 divided by 2)
 
-console.log(3 ** 2); // 3^2 = 9 (raised to the power of)
+console.log(3 ** 2); // outputs 9 (3 raised to the power of 2)
 
-// We also have shorthand (syntactic sugar) to increment or decrement a value
+// We also have shorthand operators for incrementing or decrementing values:
 let a = 10;
 
-a = a + 1;
+// Incrementing a value:
+a = a + 1;				// increases a by 1
 
-a += 1;
+a += 1;					// increases a by 1 (shorthand for a = a + 1)
 
-// This will increment the value of a, and then return the previous value.
+// Post-increment: increases a by 1, then returns the original value of a before the increment.
 a++; 
 
-// This will increment the value of a, and then return the updated value.
+// Pre-increment: increases a by 1, then returns the new value of a after the increment.
 ++a;
 
-a -= 1;
+// Decrementing a value:
+a -= 1;					// decreases a by 1
 
+// Post-decrement: decreases a by 1, then returns the original value of a before the decrement.
 a--;
 
+// Pre-increment: decreases a by 1, then returns the new value of a after the decrement.
 --a;
