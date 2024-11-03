ef79cecd3d70ba34cd6f68bf26c2e45cc8fc0a4a
Tony <160587075+42Monkey@users.noreply.github.com>
2024-11-03 18:05:30 +0700

    Update 8-logical-operators-with-non-booleans.js

--- 3 JavaScript Operators/8-logical-operators-with-non-booleans.js
+++ 3 JavaScript Operators/8-logical-operators-with-non-booleans.js
@@ -1,24 +1,29 @@
-// Expressions are evaluated from left to right.
+// Expressions are evaluated from left to right in JavaScript.
 // When using logical operators with non-boolean values,
-// rather than returning the value of true or false,
-// it will return the value of the operand.
+// they return the value of the operand instead of just true or false.
 
-// so in the case of || (or operator)
-console.log(false || 'Steven'); // returns 'Steven'
+// For the || (or) operator
+console.log(false || 'Steven');									// returns 'Steven'
 
-// So since 'Steven' is evaluated to truthy, it will be the value returned
-// the OR operator returns the first truthy value
+// Since 'Steven' is truthy, it is the value returned.
+// The OR operator returns the first truthy value it encounters.
 
-// The falsy values in JavaScript are...
-// undefined, null, 0, false, '', NaN
-// anything else that doesn't fall in this category is considered truthy
+// Falsy values in JavaScript are:
+// - undefined
+// - null
+// - 0
+// - false
+// - ''
+// - NaN
+// Any value not in this list is considered truthy.
 
-console.log(false || 1 || 2); // returns 1
-// The JavaScript OR operator, ||, performs short-circuit evaluation.
-// Meaning it stops the expression once it can evaluate to 'truthy' or 'falsy'.
+// Example of short-circuit evaluation:
+console.log(false || 1 || 2); 									// returns 1
+// The OR operator stops evaluating once it finds a truthy value.
 
-// Another example
-let usersChosenColor = 'blue';
-let defaultColor = 'green';
+// Another practical example:
+let usersChosenColor = 'blue';									// a truthy value
+let defaultColor = 'green';	
+const currentWebsiteColor = usersChosenColor || defaultColor;	// returns 'blue'
 
-const currentWebsiteColor = usersChosenColor || defaultColor;
+console.log('Current website color:', currentWebsiteColor);		// outputs: Current website color: blue
